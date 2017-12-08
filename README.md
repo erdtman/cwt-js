1bcb678f74efa587932414275f64dec8556bdcf5
Samuel Erdtman <erdtman@spotify.com>
2017-12-08 18:58:35 +0100

    added sign support

--- README.md
+++ README.md
@@ -31,13 +31,53 @@ cwt.mac(encKey, alg, kid).then(output => {
   console.log(output.toString('hex'));
 });
 ```
-# Verify MAC CWT
+# Verify MACed CWT
 ```js
 const CWT = require('cwt-js');
 const decKey = Buffer.from('403697de87af64611c1d32a05dab0fe1fcb715a86ab435f1ec99192d79569388', 'hex');
 const token = Buffer.from('d83dd18443a10104a1044c53796d6d65747269633235365850a70175636f61703a2f2f61732e6578616d706c652e636f6d02656572696b77037818636f61703a2f2f6c696768742e6578616d706c652e636f6d041a5612aeb0051a5610d9f0061a5610d9f007420b7148093101ef6d789200', 'hex');
 
 CWT.read(token, decKey).then(claims => {
+  console.log(claims);
+});
+```
+
+# Sign CWT
+```js
+const CWT = require('cwt-js');
+
+const signKey = {
+  'd': Buffer.from('6c1382765aec5358f117733d281c1c7bdc39884d04a45a1e6c67c858bc206c19', 'hex')
+};
+const alg = 'ES256';
+const kid = 'AsymmetricECDSA256';
+const claims = {
+  'iss': 'coap://as.example.com',
+  'sub': 'erikw',
+  'aud': 'coap://light.example.com',
+  'exp': 1444064944,
+  'nbf': 1443944944,
+  'iat': 1443944944,
+  'cti': '0b71'
+};
+
+const cwt = CWT.create(claims);
+cwt.sign(signKey, alg, kid).then(output => {
+  console.log(output.toString('hex'));
+});
+```
+# Verify Signed CWT
+```js
+const CWT = require('cwt-js');
+
+const verifyKey = {
+  'x': Buffer.from('143329cce7868e416927599cf65a34f3ce2ffda55a7eca69ed8919a394d42f0f', 'hex'),
+  'y': Buffer.from('60f7f1a780d8a783bfb7a2dd6b2796e8128dbbcef9d3d168db9529971a36e7b9', 'hex')
+};
+
+const token = Buffer.from('d28443a10126a104524173796d6d657472696345434453413235365850a70175636f61703a2f2f61732e6578616d706c652e636f6d02656572696b77037818636f61703a2f2f6c696768742e6578616d706c652e636f6d041a5612aeb0051a5610d9f0061a5610d9f007420b7158405427c1ff28d23fbad1f29c4c7c6a555e601d6fa29f9179bc3d7438bacaca5acd08c8d4d4f96131680c429a01f85951ecee743a52b9b63632c57209120e1c9e30', 'hex');
+
+CWT.read(token, verifyKey).then(claims => {
   console.log(claims);
 });
 ```
--- test/rfc.js
+++ test/rfc.js
@@ -7,6 +7,23 @@ const CWT = require('../');
 const jsonfile = require('jsonfile');
 
 // test('create A_3', t => {});
+test('create A_3', t => {
+  const example = jsonfile.readFileSync('test/rfc/A_3.json');
+  const key = {
+    'd': Buffer.from(example.key.d, 'hex')
+  };
+  const alg = example.alg;
+  const claims = example.claims;
+  const kid = example.key.kid;
+
+  const cwt = CWT.create(claims);
+  return cwt.sign(key, alg, kid).then(output => {
+    t.true(Buffer.isBuffer(output));
+    t.true(output.length > 0);
+    t.is(output.toString('hex'), example.output);
+  });
+});
+
 test('create A_4', t => {
   const example = jsonfile.readFileSync('test/rfc/A_4.json');
   const key = Buffer.from(example.key, 'hex');
@@ -24,9 +41,33 @@ test('create A_4', t => {
 });
 // test('create A_5', t => {});
 // test('create A_6', t => {});
-// test('create A_7', t => {});
+test('create A_7', t => {
+  const example = jsonfile.readFileSync('test/rfc/A_7.json');
+  const key = Buffer.from(example.key, 'hex');
+  const alg = example.alg;
+  const claims = example.claims;
+  const kid = example.kid;
 
-// test('verify A_3', t => {});
+  const cwt = CWT.create(claims);
+  return cwt.mac(key, alg, kid).then(output => {
+    t.true(Buffer.isBuffer(output));
+    t.true(output.length > 0);
+    t.is(output.toString('hex'), example.output);
+  });
+});
+
+test('verify A_3', t => {
+  const example = jsonfile.readFileSync('test/rfc/A_3.json');
+  const key = {
+    'x': Buffer.from(example.key.x, 'hex'),
+    'y': Buffer.from(example.key.y, 'hex')
+  };
+  const expectedClaims = example.claims;
+  const cwt = Buffer.from(example.output, 'hex');
+  return CWT.read(cwt, key).then(claims => {
+    t.deepEqual(claims, expectedClaims);
+  });
+});
 
 test('verify A_4', t => {
   const example = jsonfile.readFileSync('test/rfc/A_4.json');
@@ -37,6 +78,16 @@ test('verify A_4', t => {
     t.deepEqual(claims, expectedClaims);
   });
 });
+
 // test('verify A_5', t => {});
 // test('verify A_6', t => {});
-// test('verify A_7', t => {});
+
+test('verify A_7', t => {
+  const example = jsonfile.readFileSync('test/rfc/A_7.json');
+  const key = Buffer.from(example.key, 'hex');
+  const expectedClaims = example.claims;
+  const cwt = Buffer.from(example.output, 'hex');
+  return CWT.read(cwt, key).then(claims => {
+    t.deepEqual(claims, expectedClaims);
+  });
+});
